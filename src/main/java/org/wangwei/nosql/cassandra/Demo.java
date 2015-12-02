60de55618b1ce6c418799e82f19f81624b4c267a
Jacy Wang <jsycwangwei@icloud.com>
2015-12-02 16:28:56 +0800

    Update Demo.java

--- src/main/java/org/wangwei/nosql/cassandra/Demo.java
+++ src/main/java/org/wangwei/nosql/cassandra/Demo.java
@@ -8,18 +8,35 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Demo {
-    public static void main(String[] args) {
+     public static void main(String[] args) throws UnknownHostException {
         ConfigurableApplicationContext ct = new ClassPathXmlApplicationContext("beans.xml");
-        PersonRepository repository = ct.getBean(PersonRepository.class);
+        CassandraOperations cassandraOperations = ct.getBean("cqlTemplate", CassandraOperations.class);
+        // PersonRepository repository = ct.getBean(PersonRepository.class);
+        //
+        // List<Users> persons = new ArrayList<Users>();
+        // for (int i = 0; i < 10; i++) {
+        // Users person = new Users();
+        // person.setUserid(i);
+        // person.setCreate_time(new Date());
+        // Map<Date, String> todo = Maps.newConcurrentMap();
+        // todo.put(new Date(), "say: " + i);
+        // person.setTodo(todo);
+        // person.setFirst_name(i + "");
+        // Map<String, String> m = Maps.newConcurrentMap();
+        // m.put(i + "", "zzz");
+        // person.setKv(m);
+        // persons.add(person);
+        // Thread.currentThread().sleep(500);
+        // }
+        // repository.save(persons);
+        //
+        // ct.close();
 
-        List<Users> persons = new ArrayList<Users>();
-        for (int i = 0; i < 10; i++) {
-            Users person = new Users();
-            person.setUserid(i);
-            person.setCreate_time(new Date());
-            persons.add(person);
+        String cql = "select * from users where kv contains key '5'";
+        List<Users> us = cassandraOperations.select(cql, Users.class);
+        for (Users u : us) {
+            System.out.println(u.getUserid());
         }
-        repository.save(persons);
-        ct.close();
+
     }
 }
