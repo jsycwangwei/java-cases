8a3945a993a55ec8183252599a3a6cc73fd50421
Jacy Wang <jsycwangwei@icloud.com>
2016-01-19 14:40:03 +0800

    util demo

---
+++ src/main/java/org/wangwei/util/forkjoin/TestRecursiveAction.java
@@ -0,0 +1,58 @@
+package org.wangwei.util.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * 任务没有有返回值
+ * 
+ * @author wangwei-ww
+ */
+public class TestRecursiveAction {
+
+    public final static ForkJoinPool mainPool = new ForkJoinPool();
+
+    public static void main(String[] args) {
+        int n = 26000;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+        }
+        SubTask task = new SubTask(a, 0, n);
+        mainPool.invoke(task);
+        for (int i = 0; i < n; i++) {
+            // System.out.print(a[i] + " ");
+        }
+    }
+}
+
+class SubTask extends RecursiveAction {
+
+    private static final long serialVersionUID = 1L;
+
+    private int[] a;
+    private int beg;
+    private int end;
+
+    public SubTask(int[] a, int beg, int end) {
+        super();
+        this.a = a;
+        this.beg = beg;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if (end - beg > 10) {
+            int mid = (beg + end) / 2;
+            SubTask t1 = new SubTask(a, beg, mid);
+            SubTask t2 = new SubTask(a, mid, end);
+            invokeAll(t1, t2);
+        }
+        else {
+            for (int i = beg; i < end; i++) {
+                a[i] = a[i] + 1;
+            }
+        }
+    }
+}
