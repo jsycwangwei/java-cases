27542c301aa5633fbf44c5102ac9a94e46e3c17e
Jacy Wang <jsycwangwei@icloud.com>
2016-01-19 18:20:23 +0800

    nio

---
+++ src/main/java/org/wangwei/io/nio/sample/RspHandler.java
@@ -0,0 +1,23 @@
+package org.wangwei.io.nio.sample;
+
+public class RspHandler {
+    private byte[] rsp = null;
+
+    public synchronized boolean handleResponse(byte[] rsp) {
+        this.rsp = rsp;
+        this.notify();
+        return true;
+    }
+
+    public synchronized void waitForResponse() {
+        while (this.rsp == null) {
+            try {
+                this.wait();
+            }
+            catch (InterruptedException e) {
+            }
+        }
+
+        System.out.println(new String(this.rsp));
+    }
+}
---
+++ src/main/java/org/wangwei/io/nio/sample/ServerDataEvent.java
@@ -0,0 +1,15 @@
+package org.wangwei.io.nio.sample;
+
+import java.nio.channels.SocketChannel;
+
+class ServerDataEvent {
+    public NioServer server;
+    public SocketChannel socket;
+    public byte[] data;
+
+    public ServerDataEvent(NioServer server, SocketChannel socket, byte[] data) {
+        this.server = server;
+        this.socket = socket;
+        this.data = data;
+    }
+}
